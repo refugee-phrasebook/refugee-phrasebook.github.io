9c278ab99ca36a2143c89ddd990233e551ed7b4a
glottotopia <sebastian.nordhoff@glottotopia.de>
2015-11-07 03:24:28 +0100

    populating datatable from json

--- js/util.js
+++ js/util.js
@@ -7,13 +7,14 @@ window.onload = function(){
 
   //set table to first language in selection
 //   v = $('#lgselector').children()[0].value 
-  updatetable('eng','deu','SAMPA')
+  updatetable('spa','deu','SAMPA')
 }
 
  
 
 function updatetable(src,target,phon){ 
-    var data = json2jsdatatables(src,target,phon)
+    var data = json2jsdatatables(src,target,phon)    
+//     console.log(data)
     $('#transtable').DataTable( {
     destroy: true,
     data:data,
@@ -31,44 +32,64 @@ function updatetable(src,target,phon){
     } 
     
 function json2jsdatatables(source,target,trans){
+//  console.log(source,target,trans)
     //return array of arrays [ID, src,target,phonetic, domain]
   var lexemesjson = $.getJSON("/rpb/data/lexemes.json");
   alert(7)
-  console.log(1)
-  console.log(lexemesjson.responseText)
-  console.log(2)
+//   console.log(1)
+//   console.log(lexemesjson.responseText)
+//   console.log(2)
   lexemes = JSON.parse(lexemesjson.responseText)
 //   console.log(lexemes)
   console.log(lexemes)  
   result = []
   keys = Object.keys(lexemes)
-  console.log(keys)
-  console.log(keys.length)
+//   console.log(keys)
+//   console.log(keys.length)
   i = 0
+//   for (var i=0;i<keys.length;i++){
   for (var i=0;i<keys.length;i++){
     key = keys[i]
-    console.log(key)
+//     console.log(key)
     var lexeme = lexemes[key]
     console.log(lexeme)
+    ID = lexeme['ID']
+    console.log(ID)
+    
     ID = i
     i++
-    console.log(ID)
-    console.log(source)
-    console.log(lexeme.lgs)
-    console.log(lexeme['lgs'])
-    console.log(Object.keys(lexeme['lgs']))
-    console.log(lexeme.lgs[1])
-    console.log(lexeme.lgs.deu)
-    console.log(lexeme.lgs['deu'])
-    console.log(lexeme['lgs']['deu'])
-    console.log(lexeme['lgs'][source])
-    console.log(lexeme['lgs'].deu)
-    alert("languages are hardcoded")
-    sourcestring = lexeme['lgs']['deu']['orthographic']
-    targetstring = lexeme['lgs']['deu']['orthographic']
-    transcription =  lexeme['lgs']['deu']['transcriptions'][trans]
+//     console.log(source)
+//     console.log(target)
+//     console.log(lexeme.lgs)
+//     console.log(lexeme['lgs'])
+//     console.log(Object.keys(lexeme['lgs']))
+    keys2 = Object.keys(lexeme['lgs'])
+//     console.log(keys)
+    var targetpos = false
+    var sourcepos = false
+    for (var j = 0; j<keys2.length;j++){
+      if (source==keys2[j]){
+        sourcepos = j
+      }
+      if (target==keys2[j]){
+        targetpos=j
+      }
+    }
+//     console.log(sourcepos,targetpos)
+//     console.log(lexeme.lgs[1])
+//     console.log(lexeme.lgs.deu)
+//     console.log(lexeme.lgs['deu'])
+//     console.log(lexeme['lgs']['deu'])
+//     console.log(lexeme['lgs'][Object.keys(lexeme['lgs'])[sourcepos]])
+//     console.log(4)
+//     console.log(lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]])
+//     console.log(lexeme['lgs'].deu)
+//     alert("languages are hardcoded")
+    sourcestring = lexeme['lgs'][Object.keys(lexeme['lgs'])[sourcepos]]['orthographic']
+    targetstring = lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]]['orthographic']
+    transcription =  lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]]['transcriptions'][trans]
     domain = lexeme['domain']
-    result.push({'ID':ID,'source':sourcestring,'target':target,'phonetic':transcription,'domain':domain})
+    result.push({'ID':ID,'source':sourcestring,'target':targetstring,'phonetic':transcription,'domain':domain})
   }
   return result
 }
