74155002af70a35ef47d37bc0cd399c47b1e56e4
Schmalhans <lqfb@glottotopia.de>
2015-11-09 23:06:07 +0100

    removed superfluous console logs

--- js/util.js
+++ js/util.js
@@ -1,120 +1,89 @@
-window.onload = function(){   
+window.onload = function(){    
+  iso6393dic ={ //should be given by json, not hardcoded  FIXME
+    "hbs":"Serbo-Croatian",
+    "deu":"German",
+    "swe":"Swedish",
+    "nor":"Norwegian", 
+    "fin":"Finnish",
+    "dan":"Danish",
+    "tir":"Tigrinya",
+    "spa":"Spanish",
+    "ita":"Italian",
+    "fra":"French",
+    "ell":"Greek",
+    "hun":"Hungarian",
+    "por":"Portuguese",
+    "tam":"Tamil",
+    "pol":"Polish",
+    "rus":"Russian",
+    "isl":"Icelandic",  
+    "rom":"Romanian",
+    "ces":"Czech",
+    "slk":"Slovak",
+    "bul":"Bulgarian",
+    "cat":"Catalan",
+    "ben":"Bangla", 
+    "urd":"Urdu",
+    "amh":"Amharic",
+    "slv":"Slovenian",
+    "vie":"Vietnamese",
+    "tur":"Turkish",
+    "hye":"Armenian", 
+    "prs":"Dari",
+    "fil":"Filipino",
+    "far":"Farsi",
+    "pus":"Pashto",
+    "pes":"Persian",
+    "mkd":"Macedonian",
+    "lit":"Lithuanian",
+    "ckb":"Sorani",
+    "kmr":"Kurmanji",
+    "sqi":"Albanian",
+    "som":"Somalian",
+    "ara":"Standard Arabic",
+    "apc":"Syrian Arabic", 
+    "nld":"Dutch"    
+  } 
 
- 
-iso6393dic ={ //should be given by json, not hardcoded  FIXME
-"hbs":"Serbo-Croatian",
-"deu":"German",
-"swe":"Swedish",
-"nor":"Norwegian", 
-"fin":"Finnish",
-"dan":"Danish",
-"tir":"Tigrinya",
-"spa":"Spanish",
-"ita":"Italian",
-"fra":"French",
-"ell":"Greek",
-"hun":"Hungarian",
-"por":"Portuguese",
-"tam":"Tamil",
-"pol":"Polish",
-"rus":"Russian",
-"isl":"Icelandic",  
-"rom":"Romanian",
-"ces":"Czech",
-"slk":"Slovak",
-"bul":"Bulgarian",
-"cat":"Catalan",
-"ben":"Bangla", 
-"urd":"Urdu",
-"amh":"Amharic",
-"slv":"Slovenian",
-"vie":"Vietnamese",
-"tur":"Turkish",
-"hye":"Armenian", 
-"prs":"Dari",
-"fil":"Filipino",
-"far":"Farsi",
-"pus":"Pashto",
-"pes":"Persian",
-"mkd":"Macedonian",
-"lit":"Lithuanian",
-"ckb":"Sorani",
-"kmr":"Kurmanji",
-"sqi":"Albanian",
-"som":"Somalian",
-"ara":"Standard Arabic",
-"apc":"Syrian Arabic", 
-"nld":"Dutch"} 
+  //initialize selectors
+  keys=[] //js is too stupid to sort keys of a hashtable
+  $.each( iso6393dic, function( key, value ) { 
+      keys.push(key);
+  })
+  keys.sort();
 
-keys=[]
-$.each( iso6393dic, function( key, value ) { 
-    keys.push(key);
-})
-keys.sort();
-
-$.each( keys, function( key ) { 
-    option = '<option value="'+keys[key]+'">'+iso6393dic[keys[key]]+'</option>'
-    $("#sourceselector").append(option)
-    $("#targetselector").append(option)
-});
-  
-  
-  $( ".selector" ).change(function() { 
-    srcvalue = $("#sourceselector").val()
-    targetvalue = $("#targetselector").val()
-    transcriptionvalue = $("#transcriptionselector").val()
-    console.log(srcvalue,targetvalue,transcriptionvalue)
-    json2jsdatatables(srcvalue,targetvalue,transcriptionvalue)
+  $.each( keys, function( key ) { 
+      option = '<option value="'+keys[key]+'">'+iso6393dic[keys[key]]+'</option>'
+      $("#sourceselector").append(option)
+      $("#targetselector").append(option)
   });
-  
-  $( ".selector" ).change() //trigger initialization
-  
-
-}
-
- 
-
-// function updatetable(src,target,phon){ 
-//     var data = json2jsdatatables(src,target,phon)    
-// //     console.log(data)
-// 
-//     } 
     
-function json2jsdatatables(source,target,trans){
-//  console.log(source,target,trans)
-    //return array of arrays [ID, src,target,phonetic, domain]
+  //bind event to all selectors  
+  $( ".selector" ).change(function() { 
+      srcvalue = $("#sourceselector").val()
+      targetvalue = $("#targetselector").val()
+      transcriptionvalue = $("#transcriptionselector").val() 
+      json2jsdatatables(srcvalue,targetvalue,transcriptionvalue)
+    }); 
+  
+  //trigger initialization of DataTable
+  $( ".selector" ).change() 
+}
+ 
+    
+function json2jsdatatables(source,target,trans){ 
+  //return array of arrays [ID, src,target,phonetic, domain]
   var lexemesjson;
-  $.getJSON("./data/lexemes.json" , function( data ) { 
-//     console.log(data)
-  //   console.log(1)
-  //   console.log(lexemesjson.responseText)
-  //   console.log(2)
-    lexemes = data
-  //   console.log(lexemes)
-  //   console.log(lexemes)  
+  $.getJSON("./data/lexemes.json" , function( data ) {  
+    lexemes = data  
     result = []
-    keys = Object.keys(lexemes)
-  //   console.log(keys)
-  //   console.log(keys.length)
-    i = 0
-  //   for (var i=0;i<keys.length;i++){
+    keys = Object.keys(lexemes) 
+    i = 0 
     for (var i=0;i<keys.length;i++){
-      key = keys[i]
-  //     console.log(key)
-      var lexeme = lexemes[key]
-  //     console.log(lexeme)
-  // console.log(lexeme.ID)
-  ID = lexeme.ID
-  //     ID = i
-  //     i++
-//       console.log(ID)
-  //     console.log(source)
-  //     console.log(target)
-  //     console.log(lexeme.lgs)
-  //     console.log(lexeme['lgs'])
-  //     console.log(Object.keys(lexeme['lgs']))
-      keys2 = Object.keys(lexeme['lgs'])
-  //     console.log(keys)
+      key = keys[i] 
+      var lexeme = lexemes[key] 
+  ID = lexeme.ID 
+      keys2 = Object.keys(lexeme['lgs']) 
       var targetpos = false
       var sourcepos = false
       for (var j = 0; j<keys2.length;j++){
@@ -124,17 +93,7 @@ function json2jsdatatables(source,target,trans){
         if (target==keys2[j]){
           targetpos=j
         }
-      }
-  //     console.log(sourcepos,targetpos)
-  //     console.log(lexeme.lgs[1])
-  //     console.log(lexeme.lgs.deu)
-  //     console.log(lexeme.lgs['deu'])
-  //     console.log(lexeme['lgs']['deu'])
-  //     console.log(lexeme['lgs'][Object.keys(lexeme['lgs'])[sourcepos]])
-  //     console.log(4)
-  //     console.log(lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]])
-  //     console.log(lexeme['lgs'].deu)
-  //     alert("languages are hardcoded")
+      } 
       sourcestring = lexeme['lgs'][Object.keys(lexeme['lgs'])[sourcepos]]['orthographic']
       targetstring = lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]]['orthographic']
       transcription =  lexeme['lgs'][Object.keys(lexeme['lgs'])[targetpos]]['transcriptions'][trans]
@@ -155,12 +114,10 @@ function json2jsdatatables(source,target,trans){
             { data: 'phonetic' },
             { data: 'domain' }
           ],
-      initComplete: function(settings, json) {
-        console.log(2)
+      initComplete: function(settings, json) { 
         $('tr').click(function() {
           toggleselected($(this))
-        });  
-        console.log(3)        
+        });        
       }
     } );  
     
